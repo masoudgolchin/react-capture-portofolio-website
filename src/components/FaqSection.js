186510dcaa9d217a21a5ac3224fd89f027435b00
Masoud <masoud@markitdone.com>
2022-08-26 09:23:54 +0430

    add: components

---
+++ src/components/FaqSection.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Suestions? <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How do i start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
+            provident pariatur odit voluptas excepturi tempore possimus
+            blanditiis. Temporibus aliquid mollitia obcaecati voluptate maxime
+            dolorum aliquam? Enim illum minus dolor praesentium.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What products do you offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
+            provident pariatur odit voluptas excepturi tempore possimus
+            blanditiis. Temporibus aliquid mollitia obcaecati voluptate maxime
+            dolorum aliquam? Enim illum minus dolor praesentium.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Different payment methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
+            provident pariatur odit voluptas excepturi tempore possimus
+            blanditiis. Temporibus aliquid mollitia obcaecati voluptate maxime
+            dolorum aliquam? Enim illum minus dolor praesentium.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
+            provident pariatur odit voluptas excepturi tempore possimus
+            blanditiis. Temporibus aliquid mollitia obcaecati voluptate maxime
+            dolorum aliquam? Enim illum minus dolor praesentium.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
